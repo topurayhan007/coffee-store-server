fd34608f60d249f13449f720928f37da60102b7b
topurayhan007 <topurayhan007@gmail.com>
2023-05-11 17:27:56 +0600

    coffee get

--- index.js
+++ index.js
@@ -27,6 +27,12 @@ async function run() {
 
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
 
+    app.get("/coffee", async (req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.post("/coffee", async (req, res) => {
       const newCoffee = req.body;
       // console.log(newCoffee);
